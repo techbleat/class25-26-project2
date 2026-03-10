aac1c7e7d81e1ff6a2f2482f50978f62394a6655
Funmi123 <ajanakueniola@gmail.com>
2026-03-10 20:44:02 +0100

    codes update015

--- terraform/output.tf
+++ terraform/output.tf
@@ -16,17 +16,17 @@ output "db_endpoint" {
   value       = aws_db_instance.postgres.endpoint
 }
 
-# Generate Ansible inventory automatically
-output "inventory" {
-  description = "Dynamic Ansible inventory"
-  value = <<EOT
-[bastion]
-${aws_instance.bastion.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ec2.pem
+# # Generate Ansible inventory automatically
+# output "inventory" {
+#   description = "Dynamic Ansible inventory"
+#   value = <<EOT
+# [bastion]
+# ${aws_instance.bastion.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ec2.pem
 
-[app]
-${aws_instance.app.private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ec2.pem ansible_ssh_common_args='-o ProxyJump=ec2-user@${aws_instance.bastion.public_ip}'
-EOT
-}
+# [app]
+# ${aws_instance.app.private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ec2.pem ansible_ssh_common_args='-o ProxyJump=ec2-user@${aws_instance.bastion.public_ip}'
+# EOT
+# }
 
 # output "inventory" {
 #   value = <<EOT
