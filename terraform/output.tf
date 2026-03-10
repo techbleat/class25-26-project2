a2330f8de61397032866eb312a8252d1a08af8b6
Funmi123 <ajanakueniola@gmail.com>
2026-03-10 19:45:03 +0100

    ansible codes added 1

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,24 +1,24 @@
-# output "nginx_node_ip" {
-# value = aws_instance.nginx.public_ip
-# }
-
-# output "app_node_ip" {
-#   value = aws_instance.app.private_ip
-# }
-
-# output "bastion_node_ip" {
-#   value = aws_instance.bastion.public_ip
-# }
-
-output "inventory" {
-  value = <<EOT
-[bastion]
-${aws_instance.bastion.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
-
-[app]
-${aws_instance.app.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa ansible_ssh_common_args='-o ProxyJump=ubuntu@${aws_instance.bastion.public_ip}'
-EOT
+output "nginx_node_ip" {
+value = aws_instance.nginx.public_ip
 }
-output "db_node_ip" {
-  value = aws_db_instance.postgres.address
+
+output "app_node_ip" {
+  value = aws_instance.app.private_ip
 }
+
+output "bastion_node_ip" {
+  value = aws_instance.bastion.public_ip
+}
+
+# output "inventory" {
+#   value = <<EOT
+# [bastion]
+# ${aws_instance.bastion.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa
+
+# [app]
+# ${aws_instance.app.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa ansible_ssh_common_args='-o ProxyJump=ubuntu@${aws_instance.bastion.public_ip}'
+# EOT
+# }
+# output "db_node_ip" {
+#   value = aws_db_instance.postgres.address
+# }
