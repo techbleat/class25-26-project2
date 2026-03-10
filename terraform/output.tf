d517cb1215b9681e83cd67d6ac4dccd23602992d
Funmi123 <ajanakueniola@gmail.com>
2026-03-10 20:07:10 +0100

    codes update012

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,13 +1,31 @@
-output "nginx_node_ip" {
-value = aws_instance.nginx.public_ip
+# Bastion Public IP
+output "bastion_public_ip" {
+  description = "Public IP of Bastion server"
+  value       = aws_instance.bastion.public_ip
 }
 
-output "app_node_ip" {
-  value = aws_instance.app.private_ip
+# App Server Private IP
+output "app_private_ip" {
+  description = "Private IP of App server"
+  value       = aws_instance.app.private_ip
 }
 
-output "bastion_node_ip" {
-  value = aws_instance.bastion.public_ip
+# PostgreSQL RDS Endpoint
+output "db_endpoint" {
+  description = "PostgreSQL RDS endpoint"
+  value       = aws_db_instance.postgres.endpoint
+}
+
+# Generate Ansible inventory automatically
+output "inventory" {
+  description = "Dynamic Ansible inventory"
+  value = <<EOT
+[bastion]
+${aws_instance.bastion.public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ec2.pem
+
+[app]
+${aws_instance.app.private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ec2.pem ansible_ssh_common_args='-o ProxyJump=ec2-user@${aws_instance.bastion.public_ip}'
+EOT
 }
 
 # output "inventory" {
